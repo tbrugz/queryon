fbef77bdc0982f74b7fb75fec428f496cb7c7669
Telmo Brugnara <tbrugz@gmail.com>
2012-11-21 20:12:20 +0000

    fixed error in ResultSetFilterDecorator.absolute()

--- src/tbrugz/queryon/resultset/ResultSetFilterDecorator.java
+++ src/tbrugz/queryon/resultset/ResultSetFilterDecorator.java
@@ -17,18 +17,40 @@ public class ResultSetFilterDecorator extends AbstractResultSetDecorator {
 		//matches the shorter
 		columnsToMatch = this.colPositions.size()<this.colValues.size() ? this.colPositions.size() : this.colValues.size(); 
 	}
+
+	boolean matchesValues() throws SQLException {
+		boolean matched = true;
+		for(int i=0;i<columnsToMatch;i++) {
+			int colpos = colPositions.get(i);
+			String rsvalue = getString(colpos);
+			if(colValues.get(i).equals(rsvalue)) {}
+			else { matched = false; break; }
+		}
+		return matched;
+	}
+	
+	//XXX: only filters on next()/absolute(), no other flow method...
+	@Override
+	public boolean absolute(int row) throws SQLException {
+		/*boolean ret = super.absolute(row);
+		if(!matchesValues()) {
+			return next();
+		}
+		return ret;*/
+		boolean status = first();
+		int countMatched = 0;
+		while(countMatched < row) {
+			status = next();
+			countMatched++;
+		}
+		return status;
+	}
 	
 	@Override
 	public boolean next() throws SQLException {
 		boolean hasNext = super.next();
 		while(hasNext) {
-			boolean matched = true;
-			for(int i=0;i<columnsToMatch;i++) {
-				int colpos = colPositions.get(i);
-				String rsvalue = rs.getString(colpos);
-				if(colValues.get(i).equals(rsvalue)) {}
-				else { matched = false; break; }
-			}
+			boolean matched = matchesValues();
 			if(matched) {
 				break;
 			}
