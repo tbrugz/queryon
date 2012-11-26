e43e19541883c67cbb811d9193e1a20ad9537d30
Telmo Brugnara <tbrugz@gmail.com>
2012-11-26 22:04:14 +0000

    web: filter by FK

--- web/js/queryon.js
+++ web/js/queryon.js
@@ -7,6 +7,7 @@ var baseUrl;
 var tablesHash = {};
 var fksHash = {};
 var fksPkHash = {};
+var fksFkHash = {};
 var execsHash = {};
 
 function init(url) {
@@ -40,7 +41,7 @@ function writeTables() {
 	for(var i=0;i<tables.length;i++) {
 		var id = tables[i].schemaName+'.'+tables[i].name;
 		//$('<li><a href="'+baseUrl+'/'+id+'/select">'+id+'</li>').appendTo($('#tables'));
-		$('<li><a href="javascript:loadTable(\''+id+'\');">'+id+'</a></li>').appendTo($('#tables-content'));
+		$('<li><a href="javascript:loadTable(\''+id+'\',\'\');">'+id+'</a></li>').appendTo($('#tables-content'));
 		tablesHash[id] = tables[i];
 	}
 }
@@ -53,8 +54,13 @@ function writeFKs() {
 		fksHash[id] = fks[i];
 
 		var idPk = fks[i].pkTableSchemaName+'.'+fks[i].pkTable;
-		fksPkHash[idPk] = fks[i];
+		if(fksPkHash[idPk]==null) { fksPkHash[idPk] = []; }
+		fksPkHash[idPk].push(fks[i]);
+		var idFk = fks[i].fkTableSchemaName+'.'+fks[i].fkTable;
+		if(fksFkHash[idFk]==null) { fksFkHash[idFk] = []; }
+		fksFkHash[idFk].push(fks[i]);
 	}
+	console.log('fksHash',fksFkHash);
 }
 
 function writeExecutables() {
@@ -66,13 +72,13 @@ function writeExecutables() {
 	}
 }
 
-function loadTable(tableid) {
-	return loadTableJson(tableid);
+function loadTable(tableid, filter) {
+	return loadTableJson(tableid, filter);
 }
 
-function loadTableHtml(tableid) {
+function loadTableHtml(tableid, filter) {
 	$.ajax({
-		url: baseUrl+'/'+tableid+'/select.html',
+		url: baseUrl+'/'+tableid+'/select'+filter+'.html',
 		success: function(data) {
 			$('#main-content').html(data);
 			window.location.replace('#main-content');
@@ -81,42 +87,96 @@ function loadTableHtml(tableid) {
 	});
 }
 
-function loadTableJson(tableid) {
-	var tablename = tableid.split('.')[1];
-	console.log(tablename);
+function loadTableJson(tableid, filter) {
+	var tablearr = tableid.split('.');
+	var tableschema = tablearr[0];
+	var tablename = tablearr[1];
+	//console.log(tablename);
 	$.ajax({
-		url: baseUrl+'/'+tableid+'/select.json',
+		url: baseUrl+'/'+tableid+'/select'+filter+'.json',
 		success: function(data) {
-			console.log('success: '+tablename);
+			//console.log('success: '+tablename);
+			$('#xtraTitle').html(': '+tableid);
 			var buf = '<table>';
 			data = data[tablename];
-			var columns = tablesHash[tableid].columnNames;
-			columns = columns.replace(/[\[\]]/g, '');
 
-			var cols = columns.split(',');
+			var fks = fksFkHash[tableid];
+			console.log(tableid+': fks:: ',fks);
+
+			/*var columns = tablesHash[tableid].columnNames;
+			columns = columns.replace(/[\[\]]/g, '');
+			var cols = columns.split(',');*/
+			var cols = getColumns(tablesHash[tableid].columnNames);
+			var tablesLinks = {};
 			for(var j=0;j<cols.length;j++) {
-				cols[j] = cols[j].trim();
+				//cols[j] = cols[j].trim();
 				buf += '<th>'+cols[j]+'</th>';
+				var tableMatch = matchFkTable(fks, cols[j]);
+				//console.log(tableMatch);
+				if(tableMatch!=null) { tablesLinks[cols[j]] = tableMatch; }
 			}
-			console.log(cols);
+			console.log('cols',cols);
+			console.log('tableLinks:',tablesLinks);
 			
 			for(var i=0;i<data.length;i++) {
 				buf += '<tr>';
 				for(var j=0;j<cols.length;j++) {
-					var col = cols[j]; //.trim();
+					//XXX: construct filter URL for each FK?
+					var col = cols[j];
 					var dataz = data[i][col];
 					if(dataz!=null) {
-						buf += '<td>'+data[i][col]+'</td>';
+						//buf += '<td>';
+						if(tablesLinks[col]) {
+							buf += '<td><a href="javascript:loadTable(\''+tablesLinks[col]+'\',\'/'+data[i][col]+'\');">'+data[i][col]+'</a></td>';
+						}
+						else {
+							buf += '<td>'+data[i][col]+'</td>';
+						}
+						//buf += '</td>';
 					}
 					else {
-						buf += '<td/>';
+						buf += '<td class="tdnull"/>';
 					}
 				}
 				buf += '</tr>\n';
 			}
 			buf += '</table>';
-			$('#main-content').html(buf);
-			window.location.replace('#main-content');
+			if(filter=='') {
+				$('#main-content').html(buf);
+				window.location.replace('#main-content');
+			}
+			else {
+				var divid = 'table_'+stringToId(tableid+'_'+filter);
+				$('#main-content').append('<div id="'+divid+'" class="detail"><h4>'+tableid+' '+filter+'</h4>'+buf+'</div>');
+				window.location.replace('#'+divid);
+			}
 		}
 	});
+}
+
+function stringToId(str) {
+	return str.replace(/[\/\-\.]/g, '');
+}
+
+function getColumns(columns) {
+	columns = columns.replace(/[\[\]]/g, '');
+	var cols = columns.split(',');
+	for(var j=0;j<cols.length;j++) {
+		cols[j] = cols[j].trim();
+	}
+	return cols;
+}
+
+function matchFkTable(fks, col) {
+	if(fks!=null) {
+		for(var i=0;i<fks.length;i++) {
+			var cols = getColumns(fks[i].fkColumns);
+			var idx = $.inArray(col, cols);
+			if(idx>=0) {
+				console.log('idx', idx, fks[i].pkTable);
+				return fks[i].pkTableSchemaName+'.'+fks[i].pkTable;
+			}
+		}
+	}
+	return null;
 }
