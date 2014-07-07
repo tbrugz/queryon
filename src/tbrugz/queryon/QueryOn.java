b777daf1e691a793453fc206aecb8c9504c1c4ea
Telmo Brugnara <tbrugz@gmail.com>
2014-07-07 17:34:14 -0300

    auth: shiro: added ShiroUtils

--- src/tbrugz/queryon/QueryOn.java
+++ src/tbrugz/queryon/QueryOn.java
@@ -26,9 +26,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.PrincipalCollection;
-import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.apache.shiro.subject.Subject;
 
 import tbrugz.queryon.resultset.ResultSetFilterDecorator;
@@ -346,9 +343,9 @@ public class QueryOn extends HttpServlet {
 		}
 		
 		try {
-			Subject currentUser = getSubject();
+			Subject currentUser = ShiroUtils.getSubject(prop);
 			
-			checkPermission(currentUser, otype+":"+atype+":"+reqspec.object);
+			ShiroUtils.checkPermission(currentUser, otype+":"+atype+":"+reqspec.object);
 			switch (atype) {
 			case SELECT: {
 				Relation rel = (Relation) dbobj;
@@ -460,33 +457,6 @@ public class QueryOn extends HttpServlet {
 		relation.setQuery(sql);
 		return relation;
 	}
-	
-	// --- security-related methods <start>
-	
-	Subject getSubject() {
-		Subject currentUser = SecurityUtils.getSubject();
-		if(currentUser.getPrincipal()==null) {
-			//TODOne: get static info from properties...
-			Object userIdentity = prop.getProperty(PROP_AUTH_ANONUSER);
-			String realmName = prop.getProperty(PROP_AUTH_ANONREALM);
-			PrincipalCollection principals = new SimplePrincipalCollection(userIdentity, realmName);
-			currentUser = new Subject.Builder().principals(principals).buildSubject();
-		}
-		return currentUser;
-	}
-	
-	void checkPermission(Subject subject, String permission) {
-		//log.info("checking permission '"+permission+"', subject = "+subject);
-		if(! subject.isPermitted(permission)) {
-			log.warn("no permission '"+permission+"' for subject '"+subject+" ; "+subject.getPrincipal()+"'");
-			throw new BadRequestException(permission+": authorization required", HttpServletResponse.SC_FORBIDDEN);
-		}
-		/*else {
-			log.info("checked permission '"+permission+"' OK, subject = "+subject+" ; "+subject.getPrincipal());
-		}*/
-	}
-	
-	// --- security-related methods <end>
 	
 	void doSelect(Relation relation, RequestSpec reqspec, HttpServletResponse resp) throws IOException, ClassNotFoundException, SQLException, NamingException, ServletException {
 		Connection conn = ConnectionUtil.initDBConnection(CONN_PROPS_PREFIX, prop);
---
+++ src/tbrugz/queryon/ShiroUtils.java
@@ -0,0 +1,53 @@
+package tbrugz.queryon;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.apache.shiro.subject.Subject;
+
+public class ShiroUtils {
+
+	static final Log log = LogFactory.getLog(ShiroUtils.class);
+	
+	public static Subject getSubject(Properties prop) {
+		Subject currentUser = SecurityUtils.getSubject();
+		if(currentUser.getPrincipal()==null) {
+			//TODOne: get static info from properties...
+			Object userIdentity = prop.getProperty(QueryOn.PROP_AUTH_ANONUSER);
+			String realmName = prop.getProperty(QueryOn.PROP_AUTH_ANONREALM);
+			PrincipalCollection principals = new SimplePrincipalCollection(userIdentity, realmName);
+			currentUser = new Subject.Builder().principals(principals).buildSubject();
+		}
+		return currentUser;
+	}
+	
+	public static void checkPermission(Subject subject, String permission) {
+		//log.info("checking permission '"+permission+"', subject = "+subject);
+		if(! subject.isPermitted(permission)) {
+			log.warn("no permission '"+permission+"' for subject '"+subject+" ; "+subject.getPrincipal()+"'");
+			throw new BadRequestException(permission+": authorization required", HttpServletResponse.SC_FORBIDDEN);
+		}
+		/*else {
+			log.info("checked permission '"+permission+"' OK, subject = "+subject+" ; "+subject.getPrincipal());
+		}*/
+	}
+
+	public static void checkPermissionAny(Subject subject, String[] permissionList) {
+		for(String permission: permissionList) {
+			if(subject.isPermitted(permission)) {
+				return;
+			}
+		}
+		String permissionsStr = Arrays.asList(permissionList).toString();
+		log.warn("no permission '"+permissionsStr+"' for subject '"+subject+" ; "+subject.getPrincipal()+"'");
+		throw new BadRequestException(permissionsStr+": authorization required", HttpServletResponse.SC_FORBIDDEN);
+	}
+	
+}
