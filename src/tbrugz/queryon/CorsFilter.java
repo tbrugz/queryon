e04e96bebcd50585eba660e842ef28d1bec38c94
Telmo Brugnara <tbrugz@gmail.com>
2014-08-11 17:10:06 -0300

    queryon: added CorsFilter

---
+++ src/tbrugz/queryon/CorsFilter.java
@@ -0,0 +1,34 @@
+package tbrugz.queryon;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * see: https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+ */
+public class CorsFilter implements Filter {
+
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+	
+	public void destroy() {
+	}
+	
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+		if(resp instanceof HttpServletResponse) {
+			HttpServletResponse hresp = (HttpServletResponse) resp;
+			hresp.addHeader("Access-Control-Allow-Origin", "*");
+			hresp.addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD");
+			hresp.addHeader("Access-Control-Max-Age", "1728000"); //in seconds ; 1728000 = 20 days
+		}
+		chain.doFilter(req, resp);
+	}
+
+}
