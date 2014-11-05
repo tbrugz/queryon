3ea9c8d21927a85e9895e9d89159ac33ea4e0159
Telmo Brugnara <tbrugz@gmail.com>
2014-11-05 16:57:53 -0200

    web: graph/sigma: "animate" button on action

--- web/js/qon-util.js
+++ web/js/qon-util.js
@@ -1,7 +1,10 @@
+/* 
+ * depends on jquery
+ */
 
 function btnActionStart(btnId) {
 	$('#'+btnId).addClass('onaction');
-	//XXX add hourglass-like icon?
+	//XXX add hourglass-like icon? css transitions?
 }
 
 function btnActionStop(btnId) {
