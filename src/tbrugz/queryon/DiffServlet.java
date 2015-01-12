81968ebeb515c76ea69480fb9e23f352db639535
Telmo Brugnara <tbrugz@gmail.com>
2015-01-12 19:12:41 -0200

    qon.diff: added diffs for DBIdentifiable, using 'lastDialect'

--- src/tbrugz/queryon/DiffServlet.java
+++ src/tbrugz/queryon/DiffServlet.java
@@ -2,6 +2,7 @@ package tbrugz.queryon;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -15,11 +16,16 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.shiro.subject.Subject;
 
+import tbrugz.sqldiff.model.ChangeType;
+import tbrugz.sqldiff.model.ColumnDiff;
+import tbrugz.sqldiff.model.DBIdentifiableDiff;
 import tbrugz.sqldiff.model.Diff;
 import tbrugz.sqldiff.model.TableDiff;
+import tbrugz.sqldump.dbmd.DBMSFeatures;
 import tbrugz.sqldump.dbmodel.DBIdentifiable;
 import tbrugz.sqldump.dbmodel.SchemaModel;
 import tbrugz.sqldump.dbmodel.Table;
+import tbrugz.sqldump.def.DBMSResources;
 
 /*
  * TODO: apply diff option - add authorization like <type>:APPLYDIFF:<model>:<schema>:<name>
@@ -87,17 +93,61 @@ public class DiffServlet extends HttpServlet {
 		
 		QueryOnSchema qos = new QueryOnSchemaInstant(); // XXX use factory? new QueryOnSchema() / QueryOnSchemaInstant() ...
 		
-		DBIdentifiable dbidFrom = qos.getObject(obj.getType(), obj.getSchemaName(), obj.getName(), modelFrom, prop, modelIdFrom);
-		DBIdentifiable dbidTo = qos.getObject(obj.getType(), obj.getSchemaName(), obj.getName(), modelTo, prop, modelIdTo);
+		DBIdentifiable dbidFrom = null;
+		DBIdentifiable dbidTo = null;
 		
-		if(dbidFrom==null) {
+		//XXX: add NotFoundException (404) ?
+		try {
+			dbidFrom = qos.getObject(obj.getType(), obj.getSchemaName(), obj.getName(), modelFrom, prop, modelIdFrom);
+		}
+		catch(BadRequestException e) {
+			log.info("not found(?): "+e);
+		}
+		try {
+			dbidTo = qos.getObject(obj.getType(), obj.getSchemaName(), obj.getName(), modelTo, prop, modelIdTo);
+		}
+		catch(BadRequestException e) {
+			log.info("not found(?): "+e);
+		}
+		
+		final DBMSResources res = DBMSResources.instance();
+		final DBMSFeatures feat = res.getSpecificFeatures(qos.getLastDialect());
+		log.debug("dialect: "+qos.getLastDialect()+" feat: "+feat);
+		ColumnDiff.updateFeatures(feat);
+		//res.updateDbId(qos.getLastDialect());
+		
+		/*if(dbidFrom==null) {
 			throw new BadRequestException("Object "+obj+" not found on model "+modelIdFrom);
 		}
 		if(dbidTo==null) {
 			throw new BadRequestException("Object "+obj+" not found on model "+modelIdTo);
+		}*/
+		
+		if(dbidFrom==null && dbidTo==null) {
+			throw new BadRequestException("Object "+obj+" not found on both '"+modelIdFrom+"' and '"+modelIdTo+"' models");
 		}
 		
-		if(dbidFrom instanceof Table && dbidTo instanceof Table) {
+		if(dbidFrom==null) {
+			Diff diff = null;
+			if(dbidTo instanceof Table) {
+				diff = new TableDiff(ChangeType.ADD, (Table)dbidTo);
+			}
+			else {
+				diff = new DBIdentifiableDiff(ChangeType.ADD, null, dbidTo, null);
+			}
+			dumpDiffs(newSingleElemList(diff), resp);
+		}
+		else if(dbidTo==null) {
+			Diff diff = null;
+			if(dbidFrom instanceof Table) {
+				diff = new TableDiff(ChangeType.DROP, (Table)dbidFrom);
+			}
+			else {
+				diff = new DBIdentifiableDiff(ChangeType.DROP, dbidFrom, null, null);
+			}
+			dumpDiffs(newSingleElemList(diff), resp);
+		}
+		else if(dbidFrom instanceof Table && dbidTo instanceof Table) {
 			Table origTable = (Table) dbidFrom;
 			Table newTable = (Table) dbidTo;
 			List<Diff> diffs = TableDiff.tableDiffs(origTable, newTable);
@@ -111,15 +161,23 @@ public class DiffServlet extends HttpServlet {
 			dumpDiffs(diffs, resp);
 		}
 		else {
-			throw new BadRequestException("Object "+obj+" is not a table");
+			Diff diff = new DBIdentifiableDiff(ChangeType.REPLACE, dbidFrom, dbidTo, null);
+			dumpDiffs(newSingleElemList(diff), resp);
+			
+			//throw new BadRequestException("Object "+obj+" is not a table");
 		}
 		
 	}
+	
+	List<Diff> newSingleElemList(Diff e) {
+		List<Diff> l = new ArrayList<Diff>(); l.add(e);
+		return l;
+	}
 
 	void dumpDiffs(List<Diff> diffs, HttpServletResponse resp) throws IOException {
-		String append = ";\n";
 		for(Diff d: diffs) {
-			resp.getWriter().write(d.getDiff()+append);
+			resp.getWriter().write(d.getDiff());
+			resp.getWriter().write((d.getObjectType().isExecutableType() && d.getChangeType().equals(ChangeType.ADD))? "\n" : ";\n");
 		}
 	}
 	
