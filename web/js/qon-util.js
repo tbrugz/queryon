1729b9c270430adc1330b35a88791dd3a118dce6
Telmo Brugnara <tbrugz@gmail.com>
2016-02-04 10:49:02 -0200

    doc: qon-example update ; web: js: added escapeXML(), index update

--- web/js/qon-util.js
+++ web/js/qon-util.js
@@ -9,3 +9,13 @@ function btnActionStart(btnId) {
 function btnActionStop(btnId) {
 	document.getElementById(btnId).classList.remove("onaction");
 }
+
+// http://stackoverflow.com/a/7918944/616413
+function escapeXML(str) {
+	return str
+			.replace(/&/g, '&amp;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;')
+			.replace(/"/g, '&quot;')
+			.replace(/'/g, '&apos;');
+}
