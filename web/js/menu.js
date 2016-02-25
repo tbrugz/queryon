bc62adfbec6ef759f91be53fcf388f8644d04d81
Telmo Brugnara <tbrugz@gmail.com>
2016-02-25 13:35:15 -0300

    web: menu & index-alt changes

--- web/js/menu.js
+++ web/js/menu.js
@@ -1,15 +1,16 @@
 
 var menucontent = null;
-var nav = null;
+//var nav = null;
 
 function loadMenu() {
 	var logo = document.getElementById('logo');
-	nav = logo.parentNode;
+	var nav = logo.parentNode;
 	
 	var menu = document.createElement("i"); //<i class="fa fa-bars"></i>
 	menu.setAttribute("class", "fa fa-bars");
 	menu.setAttribute("onclick", "toggleMenu()");
 	menu.setAttribute("title", "show/hide menu");
+	menu.setAttribute("id", "menuicon");
 
 	nav.insertBefore(menu,logo);
 	
@@ -23,7 +24,11 @@ function loadMenuContent(oEvent) {
 	var txt = oEvent.target.responseText;
 	var oParser = new DOMParser();
 	var oDOM = oParser.parseFromString(txt, "text/xml");
-	menucontent = oDOM.getElementById("content")
+	menucontent = oDOM.getElementById("content");
+	//console.log(typeof loadMenuContentCallback === 'function');
+	if(typeof loadMenuContentCallback === 'function') {
+		loadMenuContentCallback();
+	}
 	//console.log(menucontent);
 	// id="content"
 }
@@ -31,7 +36,8 @@ function loadMenuContent(oEvent) {
 function toggleMenu() {
 	//console.log("toggleMenu... admin? "+authInfo.isAdmin);
 	// menucontent
-	var menuicon = nav.getElementsByTagName("i")[0];
+	var menuicon = document.getElementById("menuicon");
+	//var menuicon = nav.getElementsByTagName("i")[0];
 	var prevmenu = document.getElementById('leftmenu');
 	if(prevmenu!=null) {
 		prevmenu.parentElement.removeChild(prevmenu);
@@ -40,9 +46,22 @@ function toggleMenu() {
 		return;
 	}
 
+	var menu = createMenuContent(true);
+	menu.setAttribute("id", "leftmenu");
+
+	//menu.style.top = nav.offsetHeight + 'px';
+	//console.log("toggleMenu... admin? "+authInfo.isAdmin+" ; "+innerContent);
+
+	menuicon.classList.remove("fa-bars");
+	menuicon.classList.add("fa-arrow-left");
+	
+	var body = document.getElementsByTagName('body')[0];
+	body.appendChild(menu);
+}
+
+function createMenuContent(addCloseBtn) {
 	var lis = menucontent.getElementsByTagName('li');
 	var menu = document.createElement("div");
-	menu.setAttribute("id", "leftmenu");
 	var innerContent = '';
 	var isDeveloper = typeof authInfo != "undefined" && typeof authInfo.isAdmin != "undefined" && authInfo.isAdmin;
 	var hasLoginLink = document.getElementById('authaction') != null;
@@ -61,26 +80,26 @@ function toggleMenu() {
 			if(location.pathname.endsWith(href)) {
 				li.classList.add("current");
 			}
+			if(li.classList.contains("auth") && href.indexOf('?return')<0) {
+				href += '?return='+encodeURIComponent(window.location.href);
+				li.getElementsByTagName("a")[0].setAttribute("href", href);
+			}
 			innerContent += li.outerHTML;
 		}
 	}
 	
-	var closebtn = document.createElement("i");
-	closebtn.setAttribute("class", "fa fa-times closebtn");
-	closebtn.setAttribute("onclick", "toggleMenu()");
+	if(addCloseBtn) {
+		var closebtn = document.createElement("i");
+		closebtn.setAttribute("class", "fa fa-times closebtn");
+		closebtn.setAttribute("onclick", "toggleMenu()");
+		menu.appendChild(closebtn);
+	}
 	
 	var menuUL = document.createElement("ul");
 	menuUL.innerHTML = innerContent;
-	menu.appendChild(closebtn);
 	menu.appendChild(menuUL);
-	//menu.style.top = nav.offsetHeight + 'px';
-	//console.log("toggleMenu... admin? "+authInfo.isAdmin+" ; "+innerContent);
 
-	menuicon.classList.remove("fa-bars");
-	menuicon.classList.add("fa-arrow-left");
-	
-	var body = document.getElementsByTagName('body')[0];
-	body.appendChild(menu);
+	return menu;
 }
 
 document.addEventListener("DOMContentLoaded", loadMenu);
