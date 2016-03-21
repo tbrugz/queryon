041f3fa14c8c04d3a27f71750d3ea9bbaed0698e
Telmo Brugnara <tbrugz@gmail.com>
2016-03-21 16:11:53 -0300

    web: diff: warn if diffing same model

--- web/js/ddl.js
+++ web/js/ddl.js
@@ -1,4 +1,16 @@
 
+function showInfoMessages(messagesId, text) {
+	$('#'+messagesId).html("<span>"+text+"</span><input type='button' class='closebutton' onclick=\"javascript:closeMessages('"+messagesId+"')\" value='x' float='right'/>");
+	$('#'+messagesId).attr('class','info');
+	updateUI();
+}
+
+function showWarnMessages(messagesId, text) {
+	$('#'+messagesId).html("<span>"+text+"</span><input type='button' class='closebutton' onclick=\"javascript:closeMessages('"+messagesId+"')\" value='x' float='right'/>");
+	$('#'+messagesId).attr('class','warn');
+	updateUI();
+}
+
 function showErrorMessages(messagesId, text) {
 	var msg = document.getElementById(messagesId);
 	$('#'+messagesId).html("<span>"+text+"</span><input type='button' class='closebutton' onclick=\"javascript:closeMessages('"+messagesId+"')\" value='x' float='right'/>");
