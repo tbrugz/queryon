a97e7f0cf28b26bb8fe70281356394fc50fa733a
Telmo Brugnara <tbrugz@gmail.com>
2016-04-15 11:27:40 -0300

    web: pages-editor: added settings (.js)

---
+++ web/js/settings.js
@@ -0,0 +1,14 @@
+var settings = {};
+
+function loadSettings(callbackOk) {
+	$.ajax({
+		url: 'info/settings.jsp',
+		dataType: "text",
+		success: function(data) {
+			var info = JSON.parse(data);
+			console.log('settings',info);
+			settings = info;
+			if(callbackOk) { callbackOk(); }
+		}
+	});
+}
