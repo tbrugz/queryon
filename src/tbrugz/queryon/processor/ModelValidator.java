5b3c164eecfd5908ccebc6be4eaa3219088bdf2e
Telmo Brugnara <tbrugz@gmail.com>
2018-01-30 12:01:37 -0200

    processor: ModelValidator: added 'removeInvalid'

--- src/tbrugz/queryon/processor/ModelValidator.java
+++ src/tbrugz/queryon/processor/ModelValidator.java
@@ -1,6 +1,7 @@
 package tbrugz.queryon.processor;
 
 import java.sql.SQLException;
+import java.util.Iterator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -16,37 +17,80 @@ public class ModelValidator extends AbstractSQLProc {
 
 	static final Log log = LogFactory.getLog(ModelValidator.class);
 	
+	boolean removeInvalid = true;
+	
 	@Override
 	public void process() {
 		boolean update = true;
 		
-		try {
-			int count = 0;
-			long init = System.currentTimeMillis();
-			
-			for(Table rel: model.getTables()) {
+		int count = 0;
+		int countErr = 0;
+		long init = System.currentTimeMillis();
+		
+		// tables
+		Iterator<Table> it1 = model.getTables().iterator();
+		while(it1.hasNext()) {
+			Table rel = it1.next();
+			try {
 				DBObjectUtils.validateTable(rel, conn, update);
+			}
+			catch(SQLException e) {
+				if(removeInvalid) { it1.remove(); }
+				log.warn(e);
+				countErr++;
+			}
+			
+			count++;
+		}
+		
+		// views & queries
+		Iterator<View> it2 = model.getViews().iterator();
+		while(it2.hasNext()) {
+			View rel = it2.next();
+			if(rel instanceof Query) {
+				Query q = (Query) rel;
+				SQL sql = SQL.createSQL(q, null, null);
+				try {
+					DBObjectUtils.validateQuery(q, sql.getFinalSql(), conn, update);
+				}
+				catch(SQLException e) {
+					if(removeInvalid) { it2.remove(); }
+					log.warn(e);
+					countErr++;
+				}
 				count++;
 			}
-			for(View rel: model.getViews()) {
-				if(rel instanceof Query) {
-					Query q = (Query) rel;
-					SQL sql = SQL.createSQL(q, null, null);
-					DBObjectUtils.validateQuery(q, sql.getFinalSql(), conn, update);
-					count++;
-				}
-				else {
+			else {
+				try {
 					DBObjectUtils.validateTable(rel, conn, update);
-					count++;
 				}
+				catch(SQLException e) {
+					if(removeInvalid) { it2.remove(); }
+					log.warn(e);
+					countErr++;
+				}
+				count++;
 			}
-			//TODO: validate executables
-			
-			log.info(count+" objects validated [elapsed="+(System.currentTimeMillis()-init)+"ms]");
 		}
-		catch(SQLException e) {
-			log.warn(e);
+		
+		// executables
+		/*
+		Iterator<ExecutableObject> it3 = model.getExecutables().iterator();
+		while(it3.hasNext()) {
+			ExecutableObject eo = it3.next();
+			try {
+				DBObjectUtils.validateExecutable(eo, conn, false);
+			}
+			catch(SQLException e) {
+				if(removeInvalid) { it2.remove(); }
+				log.warn(e);
+				countErr++;
+			}
+			count++;
 		}
+		*/
+		
+		log.info((count-countErr)+" of "+count+" objects sucessfully validated [elapsed="+(System.currentTimeMillis()-init)+"ms]");
 	}
 
 }
--- src/tbrugz/queryon/util/DBObjectUtils.java
+++ src/tbrugz/queryon/util/DBObjectUtils.java
@@ -1,5 +1,6 @@
 package tbrugz.queryon.util;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ParameterMetaData;
 import java.sql.PreparedStatement;
@@ -11,12 +12,17 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import tbrugz.queryon.SQL;
 import tbrugz.sqldump.datadump.DataDumpUtils;
 import tbrugz.sqldump.dbmodel.Column;
+import tbrugz.sqldump.dbmodel.DBObjectType;
+import tbrugz.sqldump.dbmodel.ExecutableObject;
+import tbrugz.sqldump.dbmodel.ExecutableParameter;
 import tbrugz.sqldump.dbmodel.Query;
 import tbrugz.sqldump.dbmodel.Relation;
 import tbrugz.sqldump.dbmodel.Table;
 import tbrugz.sqldump.dbmodel.View;
+import tbrugz.sqldump.dbmodel.ExecutableParameter.INOUT;
 
 public class DBObjectUtils {
 
@@ -131,8 +137,95 @@ public class DBObjectUtils {
 			setColumns(rel, new ArrayList<Column>());
 			log.warn("resultset metadata's sqlexception: "+e.toString().trim());
 			log.debug("resultset metadata's sqlexception: "+e.getMessage(), e);
+			throw e;
 		}
 		
+	}
+
+	public static void validateExecutable(ExecutableObject exec, Connection conn, boolean update) throws SQLException {
+		ParameterMetaData pmd = null;
+		if(exec.getType().equals(DBObjectType.SCRIPT)) {
+			try {
+				PreparedStatement stmt = conn.prepareStatement(exec.getBody());
+				pmd = stmt.getParameterMetaData();
+			} catch (SQLException e) {
+				log.warn("parameter metadata's sqlexception [exec="+exec.getQualifiedName()+"]: "+e.toString().trim());
+				log.debug("parameter metadata's sqlexception [exec="+exec.getQualifiedName()+"]: "+e.getMessage(), e);
+				throw e;
+			}
+		}
+		else {
+			try {
+				//XXX: use JDBCSchemaGrabber's grabFunctionsColumns & grabProceduresColumns ?
+				String sql = SQL.createExecuteSQLstr(exec); // must have right number of parameters...
+				CallableStatement stmt = conn.prepareCall(sql.toString());
+				pmd = stmt.getParameterMetaData();
+				//log.info("sql: "+sql);
+			} catch (SQLException e) {
+				log.warn("parameter metadata's sqlexception [exec="+exec.getQualifiedName()+"]: "+e.toString().trim());
+				log.debug("parameter metadata's sqlexception [exec="+exec.getQualifiedName()+"]: "+e.getMessage(), e);
+				throw e;
+			}
+		}
+		
+		int pc = 0;
+		try {
+			pc = pmd.getParameterCount();
+			//log.info("addExecutable: "+exec.getQualifiedName()+" ["+exec.getType()+"]: parameter count = "+pc);
+			List<ExecutableParameter> eps = new ArrayList<ExecutableParameter>();
+			
+			for(int i=0;i<pc;i++) {
+				ExecutableParameter ep = new ExecutableParameter();
+				int pmode = ParameterMetaData.parameterModeIn; // assuming IN parameter
+				String ptype = null;
+				
+				try {
+					pmode = pmd.getParameterMode(i);
+				}
+				catch(SQLException e) {
+					//DBUtil.doRollback(conn);
+					if(logParameterMetaDataExceptions) {
+						log.warn("exception getting parameter mode [exec="+exec.getQualifiedName()+"]: "+e);
+					}
+				}
+				if(i==0 && exec.getType()==DBObjectType.FUNCTION && pmode!=ParameterMetaData.parameterModeOut) {
+					pmode = ParameterMetaData.parameterModeOut;
+					log.debug("changing return parameter mode to ParameterMetaData.parameterModeOut ["+ParameterMetaData.parameterModeOut+"]");
+				}
+				
+				try {
+					ptype = pmd.getParameterTypeName(i);
+				}
+				catch(SQLException e) {
+					//DBUtil.doRollback(conn);
+					if(logParameterMetaDataExceptions) {
+						log.warn("Exception getting parameter type [exec="+exec.getQualifiedName()+"]: "+e);
+					}
+				}
+				
+				ep.setInout(getInout(pmode));
+				if(ep.getInout()==null) {
+					log.warn("Parameter of mode '"+pmode+"' not understood for executable "+exec.getQualifiedName());
+				}
+				
+				ep.setDataType(ptype);
+				
+				//log.info("parameter: "+ep);
+				eps.add(ep);
+			}
+			if(pc!=eps.size()) {
+				log.warn("pc!=eps.size(): pc=="+pc+" ; eps.size()=="+eps.size());
+			}
+			//log.info("old params [update="+update+"]: "+exec.getParams());
+			if(update) {
+				exec.setParams(eps);
+			}
+		}
+		catch(SQLException e) {
+			log.warn("parameter metadata's sqlexception: "+e.toString().trim());
+			log.debug("parameter metadata's sqlexception: "+e.getMessage(), e);
+			throw e;
+		}
 	}
 	
 	static void setColumns(Relation rel, List<Column> columns) {
@@ -147,6 +240,21 @@ public class DBObjectUtils {
 		else {
 			log.warn("Relation '"+rel+"' not table or view");
 		}
+	}
+	
+	static INOUT getInout(int pmode) {
+		if(pmode==ParameterMetaData.parameterModeIn) {
+			return ExecutableParameter.INOUT.IN;
+		}
+		else if(pmode==ParameterMetaData.parameterModeInOut) {
+			return ExecutableParameter.INOUT.INOUT;
+		}
+		else if(pmode==ParameterMetaData.parameterModeOut) {
+			return ExecutableParameter.INOUT.OUT;
+		}
+		
+		//log.debug("Parameter of mode '"+pmode+"' not understood");
+		return null;
 	}
 	
 }
