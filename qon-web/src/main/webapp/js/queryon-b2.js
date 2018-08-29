5faad5c53628dfda416ee33d993119e7f311d523
Telmo Brugnara <tbrugz@gmail.com>
2018-08-29 13:26:00 -0300

    web: better table edit buttons layout ; index: better refresh

--- qon-web/src/main/webapp/js/queryon-b2.js
+++ qon-web/src/main/webapp/js/queryon-b2.js
@@ -251,57 +251,6 @@ function urlAddFilterOrderOffset(urlz) {
 	return urlz;
 }
 
-/*function loadAuthInfo() {
-	$.ajax({
-		url: 'auth/info.jsp',
-		dataType: "text",
-		success: function(data) {
-			var info = JSON.parse(data);
-			console.log('authInfo', info);
-			info.isAdmin = info.permissions.indexOf("SELECT_ANY")>=0;
-			info.isDev = info.isAdmin;
-			authInfo = info;
-			makeHrefs();
-		}
-	});
-}*/
-
-/*function loadSettings(callbackOk) {
-	$.ajax({
-		url: 'info/settings.jsp',
-		dataType: "text",
-		success: function(data) {
-			var info = JSON.parse(data);
-			console.log('settings',info);
-			settings = info;
-			if(callbackOk) { callbackOk(); }
-		}
-	});
-}*/
-
-/*function refreshAuthInfo() {
-	var user = document.getElementById('username');
-	user.innerHTML = authInfo.username || '';
-	var auth = document.getElementById('authaction');
-	if(! authInfo.authenticated) {
-		auth.innerHTML = '<a href="auth/login.jsp?return='+encodeURIComponent(window.location.href)+'">login</a>';
-		user.style.display = 'none';
-	}
-	else {
-		auth.innerHTML = '<a href="auth/logout.jsp?return='+encodeURIComponent(window.location.href)+'">logout</a>';
-		user.style.display = 'inline';
-	}
-
-	var urlednew = document.getElementById("url-editor-new");
-	if(authInfo.isDev && urlednew) {
-		urlednew.style.display = 'inline';
-		urlednew.href = qonEditorUrl;
-	}
-	else if(urlednew){
-		urlednew.style.display = 'none';
-	}
-}*/
-
 function getKeyValsForRow(rownum) {
 	var content = document.getElementById('content');
 	var rows = content.querySelectorAll('tr');
