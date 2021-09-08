504316c2906eade29f2fe37e4de94b6d6a866b3d
Telmo Brugnara <tbrugz@gmail.com>
2021-09-07 23:17:28 -0300

    web: intercepting inner *_HREF links and show in main UI

--- qon-web/src/main/webapp/js/queryon-b.js
+++ qon-web/src/main/webapp/js/queryon-b.js
@@ -161,7 +161,7 @@ function getQueryUrl(selectId, syntax, baseUrlParam) {
 		+(syntax?'.'+syntax:'')
 		+(queryString?'?'+queryString:'');
 	
-	console.log('getQueryUrl: url=', returl);
+	//console.log('getQueryUrl: url=', returl);
 	return returl;
 }
 
