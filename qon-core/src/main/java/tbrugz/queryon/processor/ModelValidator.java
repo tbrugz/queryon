bf2ab81f41d024438e6e33c940c9333774a89356
Telmo Brugnara <tbrugz@gmail.com>
2023-03-07 14:37:50 -0300

    processor: ModelValidator: added '.validate-executables' prop suffix

--- qon-core/src/main/java/tbrugz/queryon/processor/ModelValidator.java
+++ qon-core/src/main/java/tbrugz/queryon/processor/ModelValidator.java
@@ -9,6 +9,7 @@ import org.apache.commons.logging.LogFactory;
 
 import tbrugz.queryon.SQL;
 import tbrugz.queryon.util.DBObjectUtils;
+import tbrugz.sqldump.dbmodel.ExecutableObject;
 import tbrugz.sqldump.dbmodel.Query;
 import tbrugz.sqldump.dbmodel.Table;
 import tbrugz.sqldump.dbmodel.View;
@@ -22,15 +23,17 @@ public class ModelValidator extends AbstractSQLProc {
 	static final String PROP_PREFIX = "queryon.ModelValidator";
 
 	static final String SUFFIX_REMOVE_INVALID = ".remove-invalid-objects";
+	static final String SUFFIX_VALIDATE_EXECUTABLES = ".validate-executables";
 	
 	boolean removeInvalid = false;
 	final boolean update = true; //XXX: update may change data type & lose nullable info
-	//XXX: setInvalid...
+	boolean validateExecutables = false;
 	
 	@Override
 	public void setProperties(Properties prop) {
 		super.setProperties(prop);
 		removeInvalid = Utils.getPropBool(prop, PROP_PREFIX+SUFFIX_REMOVE_INVALID, removeInvalid);
+		validateExecutables = Utils.getPropBool(prop, PROP_PREFIX+SUFFIX_VALIDATE_EXECUTABLES, validateExecutables);
 	}
 	
 	@Override
@@ -108,7 +111,7 @@ public class ModelValidator extends AbstractSQLProc {
 		}
 		
 		// executables
-		/*
+		if(validateExecutables) {
 		Iterator<ExecutableObject> it3 = model.getExecutables().iterator();
 		while(it3.hasNext()) {
 			ExecutableObject eo = it3.next();
@@ -116,13 +119,14 @@ public class ModelValidator extends AbstractSQLProc {
 				DBObjectUtils.validateExecutable(eo, conn, false);
 			}
 			catch(SQLException e) {
+				eo.setValid(false);
 				if(removeInvalid) { it2.remove(); }
 				log.warn(e);
 				countErr++;
 			}
 			count++;
 		}
-		*/
+		}
 		
 		log.info((count-countErr)+" of "+count+" objects successfully validated [elapsed="+(System.currentTimeMillis()-init)+"ms]");
 	}
