3e8b3eaf17f1caba49a7457e32526f15476c8e33
Telmo Brugnara <tbrugz@gmail.com>
2025-03-19 20:11:53 -0300

    util: ReflectionUtil: fixed 'callProtectedMethod'

--- qon-core/src/main/java/tbrugz/queryon/util/ReflectionUtil.java
+++ qon-core/src/main/java/tbrugz/queryon/util/ReflectionUtil.java
@@ -22,8 +22,12 @@ public class ReflectionUtil {
 	}
 	
 	public static Object callProtectedMethod(Object o, String methodName, Class<?>[] parameterTypes, Object... args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Class<?> clazz = o.getClass();
+		return callProtectedMethod(clazz, o, methodName, parameterTypes, args);
+	}
+	
+	static Object callProtectedMethod(Class<?> c, Object o, String methodName, Class<?>[] parameterTypes, Object... args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Method m = null;
-		Class<?> c = o.getClass();
 		for(; m==null && !c.equals(Object.class) ; c = c.getSuperclass()) {
 			log.debug("find method "+methodName+" in class "+c.getCanonicalName());
 			try {
@@ -31,7 +35,13 @@ public class ReflectionUtil {
 			}
 			catch(NoSuchMethodException e) {
 				log.debug("... NoSuchMethodException: "+e);
-				return null;
+				/*
+				Class<?> superc = c.getSuperclass();
+				if(superc!=null && !superc.equals(Object.class)) {
+					return callProtectedMethod(superc, o, methodName, parameterTypes, args);
+				}
+				*/
+				//return null;
 			}
 			catch(RuntimeException e) {
 				log.debug("... RuntimeException: "+e);
