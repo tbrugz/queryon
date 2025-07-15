6385ed43f9825fdfd6f098e0bae307c32daca27f
Telmo Brugnara <tbrugz@gmail.com>
2025-07-15 10:59:22 -0300

    test/demo: springboot: added QonAppTest

---
+++ demo/qon-demo-springboot/src/test/java/tbrugz/queryon/springboot/demo/QonAppTest.java
@@ -0,0 +1,18 @@
+package tbrugz.queryon.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+ * see: https://spring.io/guides/gs/testing-web
+ */
+@SpringBootTest
+public class QonAppTest {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("contextLoads...");
+	}
+
+}
