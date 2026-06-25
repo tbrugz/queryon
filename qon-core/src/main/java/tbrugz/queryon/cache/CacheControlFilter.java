af724448102e296a40655a385d05092fe3261b93
Telmo Brugnara <tbrugz@gmail.com>
2026-06-24 22:13:16 -0300

    filter: CacheControlFilter: getting 'maxAge' from init parameter

--- qon-core/src/main/java/tbrugz/queryon/cache/CacheControlFilter.java
+++ qon-core/src/main/java/tbrugz/queryon/cache/CacheControlFilter.java
@@ -26,9 +26,21 @@ public class CacheControlFilter implements Filter {
 	boolean privateCache = false;
 	boolean addLastModifiedNow = false;
 	boolean addNowAsETag = true;
+	int maxAge = 3600;
 	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+		log.info("initializing CacheControlFilter");
+		String cacheMaxAge = filterConfig.getInitParameter(CACHE_MAX_AGE);
+		if(cacheMaxAge!=null) {
+			if(MiscUtils.isInt(cacheMaxAge)) {
+				maxAge = Integer.parseInt(cacheMaxAge);
+				log.info(CACHE_MAX_AGE+" = "+maxAge);
+			}
+			else {
+				log.warn("patemeter '"+CACHE_MAX_AGE+"' not an integer: "+cacheMaxAge);
+			}
+		}
 	}
 
 	@Override
@@ -40,30 +52,23 @@ public class CacheControlFilter implements Filter {
 			throws IOException, ServletException {
 		if(request instanceof HttpServletRequest) {
 			HttpServletRequest req = (HttpServletRequest) request;
-			if("GET".equals(req.getMethod())) {
-				String maxAge = req.getParameter(CACHE_MAX_AGE);
-				if(maxAge!=null) {
-					if(MiscUtils.isInt(maxAge)) {
-						HttpServletResponse resp = (HttpServletResponse) response;
-						String value = (privateCache?"private, ":"") + "max-age="+maxAge;
-						resp.setHeader("Cache-Control", value);
-						
-						if(addLastModifiedNow) {
-							Date now = new Date();
-							resp.setDateHeader("Last-Modified", now.getTime());
-						}
-						
-						if(addNowAsETag) {
-							Date now = new Date();
-							resp.setHeader("ETag", "\""+String.valueOf(now.getTime())+"\"");
-						}
-						
-						log.debug("header: Cache-Control: "+value);
-					}
-					else {
-						log.warn("patemeter '"+CACHE_MAX_AGE+"' not an integer: "+maxAge);
-					}
+			String method = req.getMethod();
+			if("GET".equals(method) || "HEAD".equals(method)) {
+				HttpServletResponse resp = (HttpServletResponse) response;
+				String value = (privateCache?"private, ":"") + "max-age="+maxAge;
+				resp.setHeader("Cache-Control", value);
+				
+				if(addLastModifiedNow) {
+					Date now = new Date();
+					resp.setDateHeader("Last-Modified", now.getTime());
 				}
+				
+				if(addNowAsETag) {
+					Date now = new Date();
+					resp.setHeader("ETag", "\""+String.valueOf(now.getTime())+"\"");
+				}
+				
+				log.debug("header: Cache-Control: "+value);
 			}
 		}
 		chain.doFilter(request, response);
